e72451fe7a7174a7c277ec51e58d09a858792601
Bohao Wang <bj050323@gmail.com>
2020-08-25 22:13:41 +0800

    hooks: useState, useCallback, useEffect, useRef, useContext, useMemo

--- src/components/ButtonGroup/index.js
+++ src/components/ButtonGroup/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Button from "../Button";
 
 export default function ButtonGroup(props) {
@@ -10,8 +10,22 @@ export default function ButtonGroup(props) {
     { id: 3, name: 'button 3' },
   ];
 
+  const [scale, setScale] = useState(1);
+  // const countRef = useRef(0);
+
+  useEffect(() => {
+    setScale(2);
+    // if (countRef.current === 0) {
+    //   setScale(2);
+    // }
+    // countRef.current++;
+    setTimeout(() => {
+      setScale(1);
+    }, 2000);
+  }, [count]);
+
   return (
-    <div>
+    <div style={{ transform: `scale(${scale})`, transformOrigin: 'left top' }}>
       <div>count: {count}</div>
       {items.map(item => (
         <Button key={item.id} onClick={() => setCount(c => c + 1)} />
