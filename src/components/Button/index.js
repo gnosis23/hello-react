a94a6f5d6afc3a656688918179f551a522cb77bd
Bohao Wang <bj050323@gmail.com>
2020-08-25 21:22:34 +0800

    init

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import React, { useCallback, useState, useEffect, useMemo, useRef } from 'react';
+
+export default function Button(props) {
+  return (
+    <button>test button</button>
+  )
+}
---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from "../../components/Button";
+
+export default function MainPage(props) {
+  return (
+    <div>
+      <Button />
+    </div>
+  )
+}
