576f417de2dcbd32fca5471f4a8cb6a86e74ec66
Bohao Wang <bj050323@gmail.com>
2021-01-25 22:31:47 +0800

    feat: cssmotion

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from "../../components/Button";
+import CSSMotionList from "../../components/CSSMotionList";
 
 export default function MainPage(props) {
   return (
     <div>
       <Button />
+      <CSSMotionList />
     </div>
   )
 }
